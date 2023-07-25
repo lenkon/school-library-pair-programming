2bed277e0cacaaf6743231b2d5166247575e293e
mohametalmeari <mohamet.almeari@gmail.com>
2023-07-25 10:35:32 +0300

    Change .upcase to .capitalize in the CapitalizeDecorator

--- person.rb
+++ person.rb
@@ -2,6 +2,9 @@ require './nameable'
 require './capitalize_decorator'
 require './trimmer_decorator'
 class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = rand(1..1000)
@@ -13,9 +16,6 @@ class Person < Nameable
   def correct_name
     @name
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   def can_use_services?
     of_age? || @parent_permission
