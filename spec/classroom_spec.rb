8e33531070903068cc455d4443c935fcf00c3883
lenkon <inoulenkon@gmail.com>
2023-08-01 17:51:05 +0530

    Fix linters errors

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,11 +1,11 @@
-require_relative '../classroom.rb'
+require_relative '../classroom'
 describe Classroom do
-    before :each do
-        @classroom = Classroom.new "Art"
+  before :each do
+    @classroom = Classroom.new 'Art'
+  end
+  context '#new' do
+    it 'takes one parameter and returns a Classroom object' do
+      @classroom.should be_an_instance_of Classroom
     end
-    context "#new" do
-         it "takes one parameter and returns a Classroom object" do
-            @classroom.should be_an_instance_of Classroom
-         end
-    end
+  end
 end
--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,16 +1,16 @@
-require_relative '../rental.rb'
-require_relative '../student.rb'
-require_relative '../book.rb'
+require_relative '../rental'
+require_relative '../student'
+require_relative '../book'
 
 describe Rental do
-    before :each do
-        @student = Student.new 15, "Student", false
-        @book = Book.new "Title", "Author"
-        @rental = Rental.new("2023/03/07", @student, @book)
+  before :each do
+    @student = Student.new 15, 'Student', false
+    @book = Book.new 'Title', 'Author'
+    @rental = Rental.new('2023/03/07', @student, @book)
+  end
+  context '#new' do
+    it 'takes three parameters and returns a Rental object' do
+      @rental.should be_an_instance_of Rental
     end
-    context "#new" do
-         it "takes three parameters and returns a Rental object" do
-            @rental.should be_an_instance_of Rental
-         end
-    end
+  end
 end
