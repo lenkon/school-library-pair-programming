68623ebbd4d5887552b9dd8e935fc80a1d5a1084
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-01 12:55:42 +0300

    Fix linter errors and failed tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,11 +1,11 @@
-require_relative '../book.rb'
+require_relative '../book'
 describe Book do
-    before :each do
-        @book = Book.new "Title", "Author"
+  before :each do
+    @book = Book.new 'Title', 'Author'
+  end
+  context '#new' do
+    it 'takes two parameters and returns a Book object' do
+      @book.should be_an_instance_of Book
     end
-    context "#new" do
-         it "takes two parameters and returns a Book object" do
-            @book.should be_an_instance_of Book
-         end
-    end
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,11 +1,11 @@
-require_relative '../person.rb'
+require_relative '../person'
 describe Person do
-    before :each do
-        @person = Person.new 18, "Person", false
+  before :each do
+    @person = Person.new 18, 'Person', parent_permission: false
+  end
+  context '#new' do
+    it 'takes three parameters and returns a Person object' do
+      @person.should be_an_instance_of Person
     end
-    context "#new" do
-         it "takes three parameters and returns a Person object" do
-            @person.should be_an_instance_of Person
-         end
-    end
+  end
 end
